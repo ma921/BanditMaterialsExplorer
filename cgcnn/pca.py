1d22f2a2097056f4e249cd210f22484247c258bf
Masaki Adachi <61237927+ma921@users.noreply.github.com>
2021-09-05 21:40:52 +0900

    Update pca.py

--- cgcnn/pca.py
+++ cgcnn/pca.py
@@ -2,7 +2,7 @@ from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('./results/cgcnn_d128_MAE134.csv', index_col=0)
+df = pd.read_csv('./cgcnn_d128_MAE134.csv', index_col=0)
 X = np.array(df)
 pca = PCA(n_components=12).fit(X)
 results = pca.transform(X)
